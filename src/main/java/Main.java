263e19b6f4eb2592bb78998135b0df4370ae0ad3
Artem Ishchenko <ext-artem.ishchenko@here.com>
2018-06-07 04:03:01 +0300

    update pom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
-    public static void main(String [] args) throws InterruptedException
+    public static void main(String [] args)
     {
         MainFrame frame = new MainFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
